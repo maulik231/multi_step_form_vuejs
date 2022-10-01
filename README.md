27ec22cf7a98ecf9d4fe0c3a88054e2a0a2e2ff9
maulik231 <maulikchodvadiya231@gmail.com>
2022-10-01 17:18:15 +0530

    created multi step form test case using cypress

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ npm run serve
 npm run build
 ```
 
+### Run your cypress tests
+```
+npx cypress open
+```
+
 ### Lints and fixes files
 ```
 npm run lint
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -5,4 +5,18 @@ describe('User Testcase', () => {
     cy.visit('http://localhost:8080/');
   });
 
+  describe('User Management: UserDetail#index', () => {
+    it('user page testing', () => {
+      cy.get('#first_name').type('Maulik');
+      cy.get('#last_name').type('Chodvadiya');
+      cy.get('.btn').click();
+
+      cy.get('#middle_name').type('Test');
+      cy.get('.next-btn').click();
+
+      cy.get('#date_of_birth').invoke('removeAttr','type').type('28/09/2022');
+      cy.get('.submit-btn').click();
+      cy.get('.confirm-btn').click();
+    })
+  });
 });
